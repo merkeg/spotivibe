bdcb9ac828544496fbb2b39969f42eb97763637e
Egor Merk <egor@merkeg.de>
2022-06-27 18:33:07 +0200

    Updates

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   outputDir: "../dist/client",
+  devServer: {
+    "^/api": {
+      target: "http://localhost:8080",
+      changeOrigin: true,
+      secure: false,
+      pathRewrite: { "^/api": "/api" },
+      logLevel: "debug",
+    },
+  },
 };
