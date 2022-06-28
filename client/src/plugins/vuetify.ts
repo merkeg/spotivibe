ed5114df9f7800a82def5e1653ecf2393289ddfc
Egor Merk <egor@merkeg.de>
2022-06-28 08:48:13 +0200

    Update

---
+++ client/src/plugins/vuetify.ts
@@ -0,0 +1,13 @@
+// Styles
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/lib/styles/main.css";
+
+// Vuetify
+import { createVuetify, ThemeDefinition } from "vuetify";
+
+export default createVuetify({
+  theme: {
+    defaultTheme: "dark",
+  },
+});
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,12 +1,19 @@
 module.exports = {
   outputDir: "../dist/client",
   devServer: {
-    "^/api": {
-      target: "http://localhost:8080",
-      changeOrigin: true,
-      secure: false,
-      pathRewrite: { "^/api": "/api" },
-      logLevel: "debug",
+    proxy: {
+      "^/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: { "^/api": "/api" },
+        logLevel: "debug",
+      },
+    },
+  },
+  pluginOptions: {
+    vuetify: {
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
     },
   },
 };
